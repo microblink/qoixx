9bcb51c7954a0bb0859d4ba4e370ebdfc693eacd
Igor Smolkovic <igor.smolkovic@microblink.com>
2024-03-12 16:52:47 +0100

    Suppress ubsan

--- include/qoixx.hpp
+++ include/qoixx.hpp
@@ -757,6 +757,9 @@ class qoi{
   static constexpr unsigned de_bruijn_bit_position_sequence[32] = {
     0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
   };
+  #ifdef __clang__
+    __attribute__((no_sanitize("undefined")))
+  #endif
   static constexpr unsigned lsb32(std::uint32_t x)noexcept{
     return de_bruijn_bit_position_sequence[(static_cast<std::uint32_t>(x&-static_cast<std::int32_t>(x))*0x077cb531u) >> 27];
   }
